0bab2604987b99cfb6c9da803533535eae406c8e
Dener Rodrigues <dl.rodrigues@gmail.com>
2020-01-03 17:31:41 -0300

    Set Production Environment

--- src/app/common/environment/env.js
+++ src/app/common/environment/env.js
@@ -1,7 +1,7 @@
 export default {
   domains: {
     development: ['localhost', '127.0.0.1'],
-    production: ['ec2-54-71-222-77.us-west-2.compute.amazonaws.com', '54.71.222.77:5000/'],
+    production: ['ec2-54-71-222-77.us-west-2.compute.amazonaws.com', '54.71.222.77'],
   },
   vars: {
     development: {
@@ -10,7 +10,7 @@ export default {
     },
     production: {
       apiUrl: '//54.71.222.77:5000',
-      baseUrl: 'http://54.71.222.77:3000',
+      baseUrl: 'http://54.71.222.77',
     },
   },
 };
