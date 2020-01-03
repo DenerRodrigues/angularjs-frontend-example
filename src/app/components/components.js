fd3e175407b4cf7b36b9b5c13024ce5a763932d4
Dener Rodrigues <dl.rodrigues@gmail.com>
2020-01-03 03:19:06 -0300

    Create SignUp module

--- src/app/components/components.js
+++ src/app/components/components.js
@@ -1,17 +1,21 @@
 import angular from 'angular';
 
 import Guest from './guest/guest';
+import SignUp from './signup/signup';
 import Login from './login/login';
 
 import Authenticated from './authenticated/authenticated';
+import Profile from './profile/profile';
 import Dashboard from './dashboard/dashboard';
 import Wish from './wish/wish';
 
 const viewsModule = angular.module('app.components', [
   Guest,
+  SignUp,
   Login,
 
   Authenticated,
+  Profile,
   Dashboard,
   Wish,
 ])
