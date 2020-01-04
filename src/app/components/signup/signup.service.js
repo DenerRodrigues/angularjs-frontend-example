697451951df097fcd829f51c925a6e07228f3c69
Dener Rodrigues <dl.rodrigues@gmail.com>
2020-01-03 23:02:29 -0300

    Create User

--- src/app/components/signup/signup.service.js
+++ src/app/components/signup/signup.service.js
@@ -1,8 +1,17 @@
-export default function SignUpService(AuthTokenService) {
+export default function SignUpService(Restangular) {
   'ngInject';
 
-  function signUp(username, password) {
-    return AuthTokenService.authToken(username, password);
+  function signUp(full_name, email, password, cep_address) {
+    const user = {
+      full_name,
+      email,
+      password,
+      cep_address,
+    };
+    return Restangular.one('signup/').customPOST(user).then((response) => {
+      this.user = JSON.parse(response).result;
+      return this.user;
+    });
   }
 
   return {
